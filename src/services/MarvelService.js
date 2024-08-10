e77c9c60e8ac3ed8cab6e35467c2ead692c14509
Sarkhan <abdullayevsarxan92@gmail.com>
2024-08-11 00:41:46 +0400

    refactoring code using machine method

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,16 +1,23 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-	const { loading, request, error, clearError } = useHttp();
+	const { request, clearError, process, setProcess } = useHttp();
 
-	const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	const _apiKey = 'apikey=22a01a5167bac3517d476b7769513bdd';
+	const _apiBase = "https://gateway.marvel.com:443/v1/public/";
+	// ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+	const _apiKey = "apikey=c5d6fc8b83116d92ed468ce36bac6c62";
 	const _baseOffset = 210;
 
 	const getAllCharacters = async (offset = _baseOffset) => {
 		const res = await request(
 			`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
 		);
+		return res.data.results.map(_transformCharacter);
+	};
+
+
+	const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
 		return res.data.results.map(_transformCharacter);
 	};
 
@@ -25,12 +32,6 @@ const useMarvelService = () => {
 		);
 		return res.data.results.map(_transformComics);
 	};
-
-	const getCharacterByName = async (name) => {
-		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
-		return res.data.results.map(_transformCharacter);
-	};
-
 
 	const getComic = async (id) => {
 		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
@@ -68,9 +69,9 @@ const useMarvelService = () => {
 	};
 
 	return {
-		loading,
-		error,
 		clearError,
+		process,
+		setProcess,
 		getAllCharacters,
 		getCharacterByName,
 		getCharacter,
